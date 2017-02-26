419091f19290936f5c2c3a8289b78dc7cfa4facc
Anchu Lee <andrew1421lee@gmail.com>
2017-02-25 21:40:04 -0500

    working

--- redditscript.js
+++ redditscript.js
@@ -39,7 +39,7 @@ function openNav(link) {
 
     var posturl = document.createElement("h3");
     posturl.setAttribute("id", "xh3");
-    posturl.innerText = link;
+    posturl.innerText = link.trunc(75);
     window.appendChild(posturl);
 
     var slider = document.createElement("INPUT");
@@ -55,15 +55,68 @@ function openNav(link) {
     textbox.setAttribute("type", "text");
     textbox.setAttribute("id","xtextbox");
     textbox.setAttribute("value", 0);
+    textbox.onchange = function(){
+        updateCashSlider(textbox.value);
+    }
     window.appendChild(textbox);
 
+    var btcontainer = document.createElement("div");
+    btcontainer.setAttribute("id", "btcontainer");
+    window.appendChild(btcontainer);
+
     var presetbuy1 = document.createElement("button");
-    presetbuy1.setAttribute("id", "xbutton");
+    presetbuy1.setAttribute("id", "preset1");
     presetbuy1.innerText = "1";
+    presetbuy1.onclick = function(){
+        updateCashSlider(1);
+        updateCashTextbox(1);
+    }
+    btcontainer.appendChild(presetbuy1);
+
+    var presetbuy2 = document.createElement("button");
+    presetbuy2.setAttribute("id", "preset2");
+    presetbuy2.innerText = "5";
+    presetbuy2.onclick = function(){
+        updateCashSlider(5);
+        updateCashTextbox(5);
+    }
+    btcontainer.appendChild(presetbuy2);
+
+    var presetbuy3 = document.createElement("button");
+    presetbuy3.setAttribute("id", "preset3");
+    presetbuy3.innerText = "20";
+    presetbuy3.onclick = function(){
+        updateCashSlider(20);
+        updateCashTextbox(20);
+    }
+    btcontainer.appendChild(presetbuy3);
+
+    var presetbuy4 = document.createElement("button");
+    presetbuy4.setAttribute("id", "preset4");
+    presetbuy4.innerText = "50";
+    presetbuy4.onclick = function(){
+        updateCashSlider(50);
+        updateCashTextbox(50);
+    }
+    btcontainer.appendChild(presetbuy4);
+
+    var presetbuy5 = document.createElement("button");
+    presetbuy5.setAttribute("id", "preset5");
+    presetbuy5.innerText = "100";
+    presetbuy5.onclick = function(){
+        updateCashSlider(100);
+        updateCashTextbox(100);
+    }
+    btcontainer.appendChild(presetbuy5);
+
+    var investbt = document.createElement("button");
+    investbt.setAttribute("id", "investbt");
+    investbt.innerText = "Purchase";
+    window.appendChild(investbt);
 
     var closebt = document.createElement("button");
-    closebt.setAttribute("id", "xbutton");
-    closebt.innerText = "cancel";
+    closebt.setAttribute("id", "closebt");
+    closebt.innerText = "Cancel";
     closebt.onclick = function(){
         closeNav();
     }
@@ -76,9 +129,18 @@ function updateCashTextbox(val){
     document.getElementById('xtextbox').value=val;
 }
 
+function updateCashSlider(val){
+    document.getElementById('xslider').value=val;
+}
+
 function closeNav(){
     var nav = document.getElementById("popupcontainer");
     nav.parentNode.removeChild(nav);
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
+
+String.prototype.trunc = String.prototype.trunc ||
+      function(n){
+          return (this.length > n) ? this.substr(0, n-1) + '[...]' : this;
+      };
