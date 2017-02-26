ce0bf7f1f1b21baaaa267b0d3bb7a167c57d12c7
Anchu Lee <andrew1421lee@gmail.com>
2017-02-25 23:19:48 -0500

    menu

--- appmenu.js
+++ appmenu.js
@@ -1,5 +1,22 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
+document.addEventListener('DOMContentLoaded', function() {
+    var uname;
+    chrome.storage.sync.get("name", function(item){
+        uname = item.name;
+        var usertext = document.getElementById("usertext");
+        usertext.innerText = uname;
+
+        var button = document.getElementById("namebutton");
+        button.onclick = function(){
+            changename();
+        }
+    });
+    
+
+    function changename(){
+        var newname = prompt("Please enter a new name", uname);
+        if(newname != null){
+            chrome.storage.sync.set({"name": newname});
+            usertext.innerText = newname;
+        }
+    }
 });
